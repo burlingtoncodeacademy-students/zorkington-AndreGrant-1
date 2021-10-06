58121da9b10d46f974b99a586c344540c8568213
Andre Grant <andrethegrant@gmail.com>
2021-10-06 17:00:55 -0400

    Reworking starting code, adding state machine

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const readline = require('readline');
-const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+const readline = require("readline");
+const readlineInterface = readline.createInterface(
+  process.stdin,
+  process.stdout
+);
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
@@ -9,12 +12,55 @@ function ask(questionText) {
 
 start();
 
+class Room {
+  constructor(description) {
+    this.description = description;
+  }
+  description() {
+    console.log(``);
+  }
+}
+
+let outside = new Room(`Dorum's Castle of The Undying.
+Woah, how did I get here again?
+You are standing in front of Dorum's castle doors.
+There is a ivory dagger stabbed into the elegant wooden doors.
+Pinned between the dagger and the door is a handwritten flyer.`);
+
+let foyer = new Room(`Dorum's Foyer.`);
+let basement = new Room(`Basement.`);
+let kitchen = new Room(`Kitchen.`);
+let upstairs = new Room(`Upstairs.`);
+let armory = new Room(`The Armory.`);
+let roof = new Room(`The Roof.`);
+
+let startPoint = "outside";
+
+let transitions = {
+  outside: ["foyer"],
+  foyer: ["basement", "kitchen", "upstairs"],
+  basement: ["foyer"],
+  kitchen: ["foyer"],
+  upstairs: ["foyer", "armory", "roof"],
+  roof: ["upstairs"],
+};
+
+function validTransition(newRoom) {
+  if (transitions[startPoint].includes(newRoom)) {
+    startPoint = newRoom;
+  } else {
+    console.log(`Not a valid room change: ${startPoint} to ${newRoom}.`);
+  }
+}
+
 async function start() {
-  const welcomeMessage = `182 Main St.
-You are standing on Main Street between Church and South Winooski.
-There is a door here. A keypad sits on the handle.
-On the door is a handwritten sign.`;
-  let answer = await ask(welcomeMessage);
-  console.log('Now write your code to make this work!');
+  const welcomeMessage = `Dorum's Castle of The Undying.
+Woah, how did I get here again?
+You are standing in front of Dorum's castle doors.
+There is a ivory dagger stabbed into the elegant wooden doors.
+Pinned between the dagger and the door is a handwritten flyer.`;
+  let answer = await ask(`${welcomeMessage}\n>_ `);
+  console.log("Now write your code to make this work!");
+
   process.exit();
 }
